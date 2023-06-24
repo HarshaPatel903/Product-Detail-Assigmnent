586f5f2906078cb2acadca2abfe821c7718e50ed
Harsha Patel <91084076+HarshaPatel903@users.noreply.github.com>
2023-06-24 23:12:39 +0530

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import Person4Icon from '@mui/icons-material/Person4';
 import SearchIcon from '@mui/icons-material/Search';
@@ -6,10 +6,24 @@ import SearchIcon from '@mui/icons-material/Search';
 import './Header.css';
 
 
-const Header = ({count}) => {
+const Header = ({count, adddToCart, setAdddToCart}) => {
+
+  const [showPopUp, setShowPopUp] = useState(false);
+  useEffect(() => {
+    if(adddToCart){
+      setShowPopUp(true); 
+      const timers = setTimeout(()=>{
+        setShowPopUp(false);
+        setAdddToCart(false)
+      },1000);
+    return () => {
+      clearTimeout(timers);
+    };
+  }
+  }, [adddToCart]);
   return (
     <header className="header">
-      <img className="header__logo"  src='https://cdn.shopify.com/s/files/1/0565/8021/0861/files/supertails-logo-for-dark-theme_200x_2x_59a36949-1934-4494-b180-9ff4bcb2cb35_230x.webp?v=1659207740' />
+      <img className="header__logo" alt='' src='https://cdn.shopify.com/s/files/1/0565/8021/0861/files/supertails-logo-for-dark-theme_200x_2x_59a36949-1934-4494-b180-9ff4bcb2cb35_230x.webp?v=1659207740' />
       <div className="header__search">
         <input type="text" placeholder="Search" />
         <button className="header__search-icon">
@@ -23,7 +37,8 @@ const Header = ({count}) => {
         <div className="header__cart_count">
             {count}
         </div>
-        <ShoppingCartIcon />
+        {showPopUp && <div className='popup'>Added to Cart</div>}
+        <ShoppingCartIcon/>
       </div>
     </header>
   );
